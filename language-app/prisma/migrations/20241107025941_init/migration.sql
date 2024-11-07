6bf4a5ac2ffd275fda59d08e8e927f368965f7d0
yusuken1121 <yusuken1121@gmail.com>
2024-11-06 19:00:08 -0800

    ✨ Create a schema

---
+++ language-app/prisma/migrations/20241107025941_init/migration.sql
@@ -0,0 +1,43 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "clerkId" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "profilePicture" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Word" (
+    "id" SERIAL NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "wordName" TEXT NOT NULL,
+    "etymology" TEXT,
+    "exampleSentence" TEXT,
+    "contextLearning" TEXT,
+    "pronunciation" TEXT,
+    "usageLevel" INTEGER NOT NULL,
+    "learningSource" INTEGER NOT NULL,
+    "learningStatus" INTEGER NOT NULL,
+    "memo" TEXT,
+    "correctCount" INTEGER NOT NULL DEFAULT 0,
+    "lastCorrectAt" TIMESTAMP(3),
+    "nextReviewAt" TIMESTAMP(3),
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Word_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_clerkId_key" ON "User"("clerkId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Word" ADD CONSTRAINT "Word_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;

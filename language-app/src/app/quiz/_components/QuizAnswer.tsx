97122938d262cb93b0ec3b255fed02f35e63ca87
yusuken1121 <yusuken1121@gmail.com>
2024-11-14 14:35:22 -0800

    ⚡️ Update the position of buttons in quiz

--- language-app/src/app/quiz/_components/QuizAnswer.tsx
+++ language-app/src/app/quiz/_components/QuizAnswer.tsx
@@ -25,30 +25,8 @@ const QuizAnswer = ({
   <>
     {currentWord && (
       <div className="relative flex flex-col items-center justify-center gap-4 bg-background w-full p-4">
-        <div className="flex items-center justify-between w-full">
-          <p>
-            正解:{" "}
-            <span className="font-bold text-2xl text-primary">
-              {currentWord?.wordName}
-            </span>
-          </p>
-        </div>
-        <BasicInfoCard
-          meaning={currentWord?.meaning || ""}
-          etymology={currentWord?.etymology || ""}
-          pronunciation={currentWord?.pronunciation || ""}
-        />
-        <LearningContextCard
-          exampleSentence={currentWord?.exampleSentence || ""}
-          contextLearning={currentWord?.contextLearning || ""}
-        />
-        <UsageInfoCard
-          synonym={currentWord?.synonym || ""}
-          formalityLevel={currentWord?.formalityLevel || ""}
-          usageArea={currentWord?.usageArea || ""}
-        />
         {/* 追加: 忘れた, 後で復習, 覚えたボタン */}
-        <div className="sticky bottom-0 bg-background bg-opacity-100 w-full flex items-center justify-center gap-4 p-2 pb-5 z-10 ">
+        <div className="sticky top-0 bg-background bg-opacity-80 w-full flex items-center justify-center gap-4 p-2 pb-5 z-10 ">
           <Button
             onClick={handleCross}
             className="bg-accent text-background hover:bg-accent/80 font-bold flex-1"
@@ -73,6 +51,28 @@ const QuizAnswer = ({
             覚えた
           </Button>
         </div>
+        <div className="flex items-center justify-between w-full">
+          <p>
+            正解:{" "}
+            <span className="font-bold text-3xl text-primary">
+              {currentWord?.wordName}
+            </span>
+          </p>
+        </div>
+        <BasicInfoCard
+          meaning={currentWord?.meaning || ""}
+          etymology={currentWord?.etymology || ""}
+          pronunciation={currentWord?.pronunciation || ""}
+        />
+        <LearningContextCard
+          exampleSentence={currentWord?.exampleSentence || ""}
+          contextLearning={currentWord?.contextLearning || ""}
+        />
+        <UsageInfoCard
+          synonym={currentWord?.synonym || ""}
+          formalityLevel={currentWord?.formalityLevel || ""}
+          usageArea={currentWord?.usageArea || ""}
+        />
       </div>
     )}
   </>

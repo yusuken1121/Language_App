c6b6d9233e352bb009a2ab390ffcf60ff987e24e
yusuken1121 <yusuken1121@gmail.com>
2024-11-20 20:33:14 -0800

    🐛 Fix an error to upload

--- language-app/src/app/word-search/page.tsx
+++ language-app/src/app/word-search/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { Suspense, useEffect, useState } from "react";
 import WordsList from "./_components/WordsList";
 import SearchBar from "./_components/SearchBar";
 
@@ -9,20 +9,22 @@ export default function WordSearch() {
   const [isSearchLoading, setIsSearchLoading] = useState<boolean>(false);
 
   return (
-    <div className="h-full p-6">
-      <SearchBar
-        setIsWordAdded={setIsWordAdded}
-        searchTerm={searchTerm}
-        setSearchTerm={setSearchTerm}
-        setIsSearchLoading={setIsSearchLoading}
-        isSearchLoading={isSearchLoading}
-      />
-      <WordsList
-        isWordAdded={isWordAdded}
-        searchTerm={searchTerm}
-        setIsWordAdded={setIsWordAdded}
-        setIsSearchLoading={setIsSearchLoading}
-      />
-    </div>
+    <Suspense>
+      <div className="h-full p-6">
+        <SearchBar
+          setIsWordAdded={setIsWordAdded}
+          searchTerm={searchTerm}
+          setSearchTerm={setSearchTerm}
+          setIsSearchLoading={setIsSearchLoading}
+          isSearchLoading={isSearchLoading}
+        />
+        <WordsList
+          isWordAdded={isWordAdded}
+          searchTerm={searchTerm}
+          setIsWordAdded={setIsWordAdded}
+          setIsSearchLoading={setIsSearchLoading}
+        />
+      </div>
+    </Suspense>
   );
 }

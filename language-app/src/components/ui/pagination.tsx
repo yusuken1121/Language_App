71e2c5c51275ea028493eb37e5df4b0937c7d1d3
yusuken1121 <yusuken1121@gmail.com>
2024-11-28 14:26:23 -0800

    💄 update the width of pagination

--- language-app/src/components/ui/pagination.tsx
+++ language-app/src/components/ui/pagination.tsx
@@ -8,7 +8,10 @@ const Pagination = ({ className, ...props }: React.ComponentProps<"nav">) => (
   <nav
     role="navigation"
     aria-label="pagination"
-    className={cn("mx-auto flex w-full justify-center", className)}
+    className={cn(
+      "mx-auto flex w-full justify-center sm:max-w-screen-sm",
+      className
+    )}
     {...props}
   />
 );
@@ -20,7 +23,10 @@ const PaginationContent = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <ul
     ref={ref}
-    className={cn("flex flex-row items-center gap-1", className)}
+    className={cn(
+      "flex flex-row items-center gap-1 sm:max-w-screen-sm",
+      className
+    )}
     {...props}
   />
 ));
@@ -65,11 +71,11 @@ const PaginationPrevious = ({
 }: React.ComponentProps<typeof PaginationLink>) => (
   <PaginationLink
     aria-label="Go to previous page"
-    size="default"
-    className={cn("gap-1 pl-2.5", className)}
+    size="icon"
+    className={cn("gap-1 pl-1", className)}
     {...props}
   >
-    <ChevronLeft className="h-4 w-4" />
+    <ChevronLeft className="h-2 w-2" />
     <span>前へ</span>
   </PaginationLink>
 );
@@ -81,12 +87,12 @@ const PaginationNext = ({
 }: React.ComponentProps<typeof PaginationLink>) => (
   <PaginationLink
     aria-label="Go to next page"
-    size="default"
-    className={cn("gap-1 pr-2.5", className)}
+    size="icon"
+    className={cn("gap-1 pr-1", className)}
     {...props}
   >
     <span>次へ</span>
-    <ChevronRight className="h-4 w-4" />
+    <ChevronRight className="h-2 w-2" />
   </PaginationLink>
 );
 PaginationNext.displayName = "PaginationNext";

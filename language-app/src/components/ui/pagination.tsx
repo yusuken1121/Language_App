2e798e78c554e5d9bb97c322a2ab258d1dc7d7c0
yusuken1121 <yusuken1121@gmail.com>
2024-11-28 14:32:12 -0800

    💄 Update the size of ellipsis in pagination

--- language-app/src/components/ui/pagination.tsx
+++ language-app/src/components/ui/pagination.tsx
@@ -103,10 +103,10 @@ const PaginationEllipsis = ({
 }: React.ComponentProps<"span">) => (
   <span
     aria-hidden
-    className={cn("flex h-9 w-9 items-center justify-center", className)}
+    className={cn("flex h-6 w-6 items-center justify-center", className)}
     {...props}
   >
-    <MoreHorizontal className="h-4 w-4" />
+    <MoreHorizontal className="h-3 w-3" />
     <span className="sr-only">More pages</span>
   </span>
 );

845743f40e031593693e7101e583295c2f239e55
yusuken1121 <yusuken1121@gmail.com>
2024-11-27 16:45:52 -0800

    🐛 Add console log to find a bug

--- language-app/src/app/api/chat/route.ts
+++ language-app/src/app/api/chat/route.ts
@@ -7,13 +7,16 @@ export const maxDuration = 30;
 
 export async function POST(req: NextRequest) {
   const { messages }: { messages: CoreMessage[] } = await req.json(); //[ { role: '', content: '' } ]
-  const messagesText = messages.map((msg) => msg.content).join(", ");
+  console.log("⭐️", messages);
 
+  const messagesText = messages.map((msg) => msg.content).join(", ");
+  console.log("⭐️", "messageText", messagesText);
   const result = streamText({
     model: google("gemini-1.5-flash-latest"),
     system: "You are a helpful assistant for Japanese English learner",
     prompt: `日本語学習者のために英語を教えています。日本語の単語やフレーズ「${messagesText}」に関連する、一般的な英語のフレーズを3つ教えてください。最もよく使われるものから順に列挙してください。必要であれば、簡単な説明も追加してください。日本語で答えてください。`,
   });
+  console.log("🚀", result);
 
   return result.toDataStreamResponse();
 }

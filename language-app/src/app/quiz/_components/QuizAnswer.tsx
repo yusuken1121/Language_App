48840fde5f38f6f459a0e0319842275651c9f6b3
yusuken1121 <yusuken1121@gmail.com>
2024-11-29 18:56:11 -0800

    💄 Update the size of the letter in quiz

--- language-app/src/app/quiz/_components/QuizAnswer.tsx
+++ language-app/src/app/quiz/_components/QuizAnswer.tsx
@@ -26,7 +26,7 @@ const QuizAnswer = ({
     {currentWord && (
       <div className="relative flex flex-col items-center justify-center gap-4 bg-background w-full p-2">
         {/* 追加: 忘れた, 後で復習, 覚えたボタン */}
-        <div className="sticky top-0 bg-background bg-opacity-80 w-full flex items-center justify-center gap-2 p-2 pb-5 z-10 ">
+        <div className="sticky top-0 bg-background bg-opacity-80 w-full flex items-center justify-center gap-2 p-2 pb-5 z-10 text-sm">
           <Button
             onClick={handleCross}
             className="bg-accent text-background hover:bg-accent/80 font-bold flex-1"

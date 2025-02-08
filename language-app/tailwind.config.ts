91bc756eb06e2e3b0e779dce40b53f994ce35545
yusuken1121 <yusuken1121@gmail.com>
2025-02-07 17:46:31 -0800

    ⚡️ Add white background

--- language-app/tailwind.config.ts
+++ language-app/tailwind.config.ts
@@ -26,6 +26,7 @@ const config: Config = {
         },
         secondary: {
           DEFAULT: "hsl(var(--secondary))",
+          background: "hsl(var(--secondary-background))",
           foreground: "hsl(var(--secondary-foreground))",
         },
         muted: {
